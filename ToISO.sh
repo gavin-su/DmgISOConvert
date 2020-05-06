9875864e00619a6cc403fb3871e9f78b37723e7c
suguoli <su.guoli@foxmail.com>
2020-05-06 15:29:12 +0800

    tmp ci

--- ToISO.sh
+++ ToISO.sh
@@ -10,22 +10,20 @@ MACOS=$1
 DMGFILE=$2
 OUTDIR=$3
 
-hdiutil create -o /tmp/${MACOS} -size 9000m -volume ${MACOS} -layout SPUD -fs HFS+J
+hdiutil create -o /tmp/${MACOS} -size 9000m -layout SPUD -fs HFS+J
 TMPFILE="/tmp/${MACOS}.dmg"
 hdiutil attach ${TMPFILE} -noverify -mountpoint /Volumes/${MACOS}
 
 hdiutil attach ${DMGFILE} -mountpoint /Volumes/os
-APP=`ls -1 /Volumes/os | grep \.app`
+APP=`ls -1 /Volumes/os | grep \.app | grep Install`
 
 #sudo /Volumes/os/${APP}/Contents/Resources/createinstallmedia --volume /Volumes/MyVolume --applicationpath /Applications/Install\ OS\ X\ El\ Capitan.app
 #sudo /Volumes/os/${APP}/Contents/Resources/createinstallmedia --volume /Volumes/MyVolume --applicationpath /Applications/Install\ macOS\ Sierra.app
 sudo /Volumes/os/${APP}/Contents/Resources/createinstallmedia --volume /Volumes/${MACOS} --nointeraction
 
 hdiutil detach /Volumes/Install\ macOS\ ${MACOS}
-
 hdiutil detach /Volumes/os
 
 hdiutil convert ${TMPFILE} -format UDTO -o ${OUTDIR}/${MACOS}.cdr
-
 mv ${OUTDIR}/${MACOS}.cdr ${OUTDIR}/${MACOS}.iso
 rm ${TMPFILE}
