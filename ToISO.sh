7cab4b11af5718b8472544c4e53a6d9c05fa03ee
suguoli <su.guoli@foxmail.com>
2020-05-06 15:21:06 +0800

    tmp ci

--- ToISO.sh
+++ ToISO.sh
@@ -1,24 +1,31 @@
 #!/bin/bash
 
-if [ $# != 2 ] ; then
-    echo "USAGE: $0 macos dmg_path"
-    echo " e.g.: $0 Catalina /tmp/Catalina.dmg"
+if [ $# != 3 ] ; then
+    echo "USAGE: $0 macos dmg_file out_dir"
+    echo " e.g.: $0 Catalina ~/Downloads/Catalina.dmg ~/Downloads"
     exit 1;
 fi
 
 MACOS=$1
-DMGPATH=$2
+DMGFILE=$2
+OUTDIR=$3
 
-hdiutil create -o /tmp/${MACOS} -size 9000m -layout SPUD -fs HFS+J
+hdiutil create -o /tmp/${MACOS} -size 9000m -volume ${MACOS} -layout SPUD -fs HFS+J
+TMPFILE="/tmp/${MACOS}.dmg"
+hdiutil attach ${TMPFILE} -noverify -mountpoint /Volumes/${MACOS}
 
-hdiutil attach ${DMGPATH} -noverify -mountpoint /Volumes/install_build
+hdiutil attach ${DMGFILE} -mountpoint /Volumes/os
+APP=`ls -1 /Volumes/os | grep \.app`
 
-#sudo /Applications/Install\ macOS\ ${MACOS}.app/Contents/Resources/createinstallmedia --volume /Volumes/install_build
-sudo /Volumes/install_build/Install\ macOS\ ${MACOS}.app/Contents/Resources/createinstallmedia --volume /Volumes/install_build
+#sudo /Volumes/os/${APP}/Contents/Resources/createinstallmedia --volume /Volumes/MyVolume --applicationpath /Applications/Install\ OS\ X\ El\ Capitan.app
+#sudo /Volumes/os/${APP}/Contents/Resources/createinstallmedia --volume /Volumes/MyVolume --applicationpath /Applications/Install\ macOS\ Sierra.app
+sudo /Volumes/os/${APP}/Contents/Resources/createinstallmedia --volume /Volumes/${MACOS} --nointeraction
 
-#hdiutil detach /Volumes/Install\ macOS\ ${MACOS}
-hdiutil detach /Volumes/install_build
+hdiutil detach /Volumes/Install\ macOS\ ${MACOS}
 
-hdiutil convert ${DMGPATH} -format UDTO -o ~/Downloads/${MACOS}
+hdiutil detach /Volumes/os
 
-mv ~/Downloads/${MACOS}.cdr ~/Downloads/${MACOS}.iso
+hdiutil convert ${TMPFILE} -format UDTO -o ${OUTDIR}/${MACOS}.cdr
+
+mv ${OUTDIR}/${MACOS}.cdr ${OUTDIR}/${MACOS}.iso
+rm ${TMPFILE}
