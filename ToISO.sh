8bafaa1f454ebced34d156fbdabc0609f457b5ca
suguoli <su.guoli@foxmail.com>
2020-05-07 16:09:01 +0800

    Sierra ok

--- ToISO.sh
+++ ToISO.sh
@@ -11,7 +11,7 @@ DMGFILE=$2
 OUTDIR=$3
 
 #check
-case ${MACOS} in
+case "${MACOS}" in
     Catalina)
     ;;
     Mojave)
@@ -31,46 +31,46 @@ esac
 
 if [ ! -e "${DMGFILE}" ]
 then
-    echo "ERROR!!! ${DMGFILE} NOT FOUND!!!"
+    echo "ERROR!!! "${DMGFILE}" NOT FOUND!!!"
     exit 1;
 fi
 
 if [ ! -d "${OUTDIR}" ]
 then
-    echo "ERROR!!! ${OUTDIR} NOT FOUND!!!"
+    echo "ERROR!!! "${OUTDIR}" NOT FOUND!!!"
     exit 1;
 fi
 
 #convert
-hdiutil create -o /tmp/${MACOS} -size 9000m -layout SPUD -fs HFS+J
+hdiutil create -o "/tmp/${MACOS}" -size 9000m -layout SPUD -fs HFS+J
 TMPFILE="/tmp/${MACOS}.dmg"
-hdiutil attach ${TMPFILE} -noverify -mountpoint "/Volumes/${MACOS}"
+hdiutil attach "${TMPFILE}" -noverify -mountpoint "/Volumes/${MACOS}"
 
-hdiutil attach ${DMGFILE} -mountpoint /Volumes/os
+hdiutil attach "${DMGFILE}" -mountpoint /Volumes/os
 APP=`ls -1 /Volumes/os | grep \.app | grep Install`
 
 
-case ${MACOS} in
+case "${MACOS}" in
     Sierra)
-    sudo "/Volumes/os/${APP}/Contents/Resources/createinstallmedia"  --volume "/Volumes/${MACOS}" --applicationpath /Applications/Install\ macOS\ Sierra.app --nointeraction
+    sudo "/Volumes/os/${APP}/Contents/Resources/createinstallmedia"  --volume "/Volumes/${MACOS}" --applicationpath "/Volumes/os/${APP}" --nointeraction
     ;;
     "El Capitan")
-    sudo "/Volumes/os/${APP}/Contents/Resources/createinstallmedia"  --volume "/Volumes/${MACOS}" --applicationpath /Applications/Install\ OS\ X\ El\ Capitan.app -nointeraction 
+    sudo "/Volumes/os/${APP}/Contents/Resources/createinstallmedia"  --volume "/Volumes/${MACOS}" --applicationpath "/Volumes/os/${APP}" --nointeraction
     ;;
     Yosemite)
+    sudo "/Volumes/os/${APP}/Contents/Resources/createinstallmedia"  --volume "/Volumes/${MACOS}" --applicationpath "/Volumes/os/${APP}" --nointeraction
     ;;
     *)
-    sudo "/Volumes/os/${APP}/Contents/Resources/createinstallmedia" --volume "/Volumes/${MACOS}" --nointeraction
+    sudo "/Volumes/os/${APP}/Contents/Resources/createinstallmedia"  --volume "/Volumes/${MACOS}" --nointeraction
     ;;
 esac
 
-
-INSTVOL=`ls -1 /Volumes | grep Install`
+INSTVOL=`ls -1 /Volumes | grep Install | grep "${MACOS}"`
 hdiutil detach "/Volumes/${INSTVOL}"
 hdiutil detach /Volumes/os
 
-hdiutil convert ${TMPFILE} -format UDTO -o ${OUTDIR}/${MACOS}.cdr
-mv ${OUTDIR}/${MACOS}.cdr ${OUTDIR}/${MACOS}.iso
+hdiutil convert "${TMPFILE}" -format UDTO -o "${OUTDIR}/${MACOS}.cdr"
+mv "${OUTDIR}/${MACOS}.cdr" "${OUTDIR}/${MACOS}.iso"
 
 #clean
-rm ${TMPFILE}
+rm "${TMPFILE}"
